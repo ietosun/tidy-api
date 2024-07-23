29a00fb9bc25f34a896ebd13126c1f2405f837f8
bebo0 <bebo007@daonplace.com>
2024-07-23 21:15:46 +0900

    - Init Project

---
+++ src/main/java/ietosun/tidyapi/TidyApiApplication.java
@@ -0,0 +1,13 @@
+package ietosun.tidyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TidyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TidyApiApplication.class, args);
+    }
+
+}
